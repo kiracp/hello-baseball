92c6675b00c4b049a31066b16a64fdbda13bb312
Fabio Elia <fabio@f1v.co>
2019-03-09 13:13:21 -0500

    Add additional helpers

--- app/functions/helpers.js
+++ app/functions/helpers.js
@@ -1,16 +1,64 @@
-const _ = require('lodash');
+const _ = require("lodash");
 
-exports.findGameResultsForTeamDate = (team, date) => {
+const findTeamName = teamKey => {
+  const teamData = require("./data/id_to_name.json");
+  return teamData[teamKey][0];
+};
+exports.findTeamName = findTeamName;
+
+const findTeamKey = teamName => {
+  const teamData = require("./data/name_to_id.json");
+  return teamData[teamName][0];
+};
+exports.findTeamKey = findTeamKey;
+
+const findGameResultsForTeamDate = (team, date) => {
   const teamData = require(`./data/teams/${team}.json`);
   const year = _.split(date, "-")[0];
   const allGames = teamData[year];
 
-  const gameData = _.find(allGames, { "date_game": date });
+  const gameData = _.find(allGames, { date_game: date });
 
   return gameData;
-}
+};
+exports.findGameResultsForTeamDate = findGameResultsForTeamDate;
 
-exports.findTeamKey = (teamName) => {
-  const teamData = require('./data/name_to_id.json');
-  return _.lowerCase(teamData[teamName][0]);
-}
+const gameFacts = gameData => {
+  const {
+    winning_pitcher,
+    win_loss_result,
+    losing_pitcher,
+    extra_innings,
+    win_loss_streak,
+    team_ID,
+    opp_ID,
+    attendance,
+    time_of_game
+  } = gameData;
+
+  const hasWinStreak = win_loss_streak.includes("+");
+  const winningTeam =
+    win_loss_result === "W" ? findTeamName(team_ID) : findTeamName(opp_ID);
+  const losingTeam =
+    win_loss_result !== "W" ? findTeamName(team_ID) : findTeamName(opp_ID);
+
+  return {
+    opponent: ` vs ${findTeamName(opp_ID)}`,
+    time: `the game started at ${time_of_game}`,
+    attendance: `${attendance} folks attended`,
+    streak: `with a ${hasWinStreak ? "winning" : "losing"} streak of ${
+      win_loss_streak.length
+    }`,
+    pitcher: `won by ${winning_pitcher}, the ${winningTeam}`,
+  };
+};
+exports.gameFacts = gameFacts;
+
+exports.dictionaryMap = key => {
+  const mapFile = {
+    W: ["beat", "won"],
+    L: ["lost to"]
+  };
+
+  return _.sample(mapFile[key]);
+};
